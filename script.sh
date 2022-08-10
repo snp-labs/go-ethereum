844e56bc1c77c79f0b7c5db96fcbf225606e9e51
hardice501 <kr2519@kakao.com>
2022-08-10 09:57:19 +0900

    script: gasCap 변경 5천만 -> 30억

--- script.sh
+++ script.sh
@@ -69,8 +69,10 @@ WSADDR="127.0.0.1"    # --ws.addr
 WSAPI="admin,eth,debug,miner,net,txpool,personal,web3"      # --ws.api
 VERBOSITY=3                 # --verbosity  If you want show geth's log, changed this value to 3
 MINERTHREADS=1              # --miner.threads
-DEFALTOPTION="--verbosity ${VERBOSITY} --allow-insecure-unlock --unlock 0 --password password --mine --miner.threads ${MINERTHREADS} --miner.gaslimit 3000000000"
-            # |           LOG               unlock-account        (password file)  |               mine option
+GASLIMIT=3000000000
+INSECURE="--allow-insecure-unlock --unlock 0 --password password"
+DEFALTOPTION="--verbosity ${VERBOSITY} --mine --miner.threads ${MINERTHREADS} --miner.gaslimit ${GASLIMIT} --rpc.gascap ${GASLIMIT}"
+            # |           LOG         |               mine option             |                     gas limit                      |
 
 #COMMANDS
 REMOVE="rm -rf home_geth/geth && rm -rf geth.log"
